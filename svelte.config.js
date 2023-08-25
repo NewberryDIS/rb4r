151a4e7f74621cb03c20e3bc5248cb27f8328270
nick3white <nick33white@gmail.com>
2023-08-24 19:19:16 -0500

    addd prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -3,26 +3,26 @@ import adapter from "@sveltejs/adapter-vercel";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // prerender: {
-    //   handleHttpError: "warn",
-    //   entries: [
-    //     "/",
-    //     "/en",
-    //     "/es",
-    //     "/en/storymap",
-    //     "/es/storymap",
-    //     "/en/storymap/france",
-    //     "/en/storymap/germany",
-    //     "/en/storymap/turkey",
-    //     "/en/storymap/mexico",
-    //     "/en/storymap/england",
-    //     "/es/storymap/france",
-    //     "/es/storymap/germany",
-    //     "/es/storymap/turkey",
-    //     "/es/storymap/mexico",
-    //     "/es/storymap/england",
-    //   ],
-    // },
+    prerender: {
+      handleHttpError: "warn",
+      entries: [
+        "/",
+        "/en",
+        "/es",
+        "/en/storymap",
+        "/es/storymap",
+        "/en/storymap/france",
+        "/en/storymap/germany",
+        "/en/storymap/turkey",
+        "/en/storymap/mexico",
+        "/en/storymap/england",
+        "/es/storymap/france",
+        "/es/storymap/germany",
+        "/es/storymap/turkey",
+        "/es/storymap/mexico",
+        "/es/storymap/england",
+      ],
+    },
     adapter: adapter({
       runtime: "nodejs18.x",
     }),
