3013be17f558352f8aaa854c745c8cd52835d14f
Nick White <nick33white@gmail.com>
2023-08-25 12:44:39 -0500

    rebuild routing - 2x optional params with matches, remove '/en' routes

--- svelte.config.js
+++ svelte.config.js
@@ -4,31 +4,29 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   kit: {
     prerender: {
-      handleHttpError: "warn",
       entries: [
         "/",
-        "/en",
         "/es",
-        "/en/storymap",
-        "/es/storymap",
-        "/en/storymap/france",
-        "/en/storymap/germany",
-        "/en/storymap/turkey",
-        "/en/storymap/mexico",
-        "/en/storymap/england",
-        "/es/storymap/france",
-        "/es/storymap/germany",
-        "/es/storymap/turkey",
-        "/es/storymap/mexico",
-        "/es/storymap/england",
+        "/france",
+        "/germany",
+        "/turkey",
+        "/mexico",
+        "/england",
+        "/es/france",
+        "/es/germany",
+        "/es/turkey",
+        "/es/mexico",
+        "/es/england",
       ],
     },
-    adapter: adapter(),
+    adapter: adapter({
+      base: "/rb4r",
+    }),
     paths: {
       base: "/rb4r",
     },
     alias: {
-      $comps: "src/lib/components/",
+      $comps: "src/lib/components",
     },
   },
 };
