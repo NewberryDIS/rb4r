549962bbec2f0e5688a21739d0ea926a04c14c57
Nick White <nick33white@gmail.com>
2023-08-28 11:40:39 -0500

    more feedback implementation

--- src/lib/components/circles.svelte
+++ src/lib/components/circles.svelte
@@ -1,25 +1,20 @@
 
 <script>
-    export let activeValue, circe, idx
-    function handleTap (int){
-        activeValue = activeValue == int ? 99 : int
-    }
-    function handleKeyup(e, int){
-        if ( e.key ==='Enter' || e.key === 'Space'  ){
-            activeValue = activeValue == int ? 99 : int
-        }
-    }
+    export let cardHover, circe, idx
     const textPos = parseInt(circe.y) + 12
 </script>
-<div class="circle-wrap">
+<div class="circle-wrap {cardHover === idx ? 'card-hover' : ''}">
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1452 1056">
-        <circle r="25" cx={circe.x} cy={circe.y} fill="#{circe.color}" role="navigation" aria-label="show navigation" on:click={() => handleTap(idx)} on:keyup={() => handleKeyup(idx)} />
+        <circle r="25" cx={circe.x} cy={circe.y} fill="#{circe.color}" role="navigation" aria-label="show navigation" on:mouseenter={() => cardHover = idx} on:mouseleave={() => cardHover = 99} />
         <text x={circe.x} y={textPos} text-anchor="middle" stroke="rgb(var(--midnight))" stroke-width="2px" font-size="37px">{idx + 1}</text>
     </svg>
 
 </div>
 
 <style>
+    .card-hover {
+        filter: brightness(1.5);
+    }
 .circle-wrap {
     position: fixed;
     /* top: 0; */
@@ -30,6 +25,7 @@
     width: 100vw;
     /* height: calc(100vh - 75px); */
     pointer-events: none;
+        transition: 200ms;
 }
 svg {
     width: 100vw;
