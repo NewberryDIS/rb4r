144ae362e8978fffc94d03f68e595efa98c86543
Nick White <nick33white@gmail.com>
2023-09-07 01:53:12 -0500

    add thanks section, change france -> belgium

--- src/params/id.js
+++ src/params/id.js
@@ -1,3 +1,3 @@
 export function match(param) {
-  return ["england", "france", "germany", "mexico", "turkey"].includes(param);
+  return ["england", "belgium", "germany", "mexico", "turkey"].includes(param);
 }
